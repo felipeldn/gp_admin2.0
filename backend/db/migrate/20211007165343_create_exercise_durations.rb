10ab4a37519903f696cd6f7466b324b1224ce5a7
felipeldn91 <felipeldn91@gmail.com>
2021-10-07 18:10:07 +0100

    migrations done

--- backend/db/migrate/20211007165343_create_exercise_durations.rb
+++ backend/db/migrate/20211007165343_create_exercise_durations.rb
@@ -2,7 +2,7 @@ class CreateExerciseDurations < ActiveRecord::Migration[6.0]
   def change
     create_table :exercise_durations do |t|
       t.integer :value
-      t.timestamps :date_time
+      t.timestamp :date_time
       t.belongs_to :patient, null: false, foreign_key: true
 
       t.timestamps
